6f4b647729faf310a292156139848090a215f4fc
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2023-05-04 22:21:08 -0500

    error

--- BoozeBear/App.js
+++ BoozeBear/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Agregar from './Componentes/Boton_Agregar';
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Agregar/>
+      
      <Text style={styles.Text} >Mi primer aplicación :D</Text>
       <Text style={styles.Text} >Trabajo y Rectitud</Text>
       <StatusBar style="auto" />
