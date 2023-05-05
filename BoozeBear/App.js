2ba7848acadc4832d44df057c8f1ff1c8ad74713
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2023-05-04 21:47:46 -0500

    voR

--- BoozeBear/App.js
+++ BoozeBear/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Agregar from './Componentes/Boton_Agregar';
+import V_Or_R from './Componentes/Botones_categorias';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Agregar/>
+      <V_Or_R/>
       <Text style={styles.Text} >Mi primer aplicación :D</Text>
-      <Text style={styles.Text} >Trabajo y Rectitud</Text>
+      <Text style={styles.Text} >Trabajo y Rectitud amen</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ BoozeBear/Componentes/Botones_Categorias.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { __interactionsRef } from 'scheduler/tracing';
+
+export default function V_Or_R (){
+    return(
+       <view>
+        <Text>Verdad o Reto</Text>
+       </view>
+
+    );
+}
