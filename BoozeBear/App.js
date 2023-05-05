a5fe3c3ce02f64f1c4fbd4ab2ddfaa7b99b11b77
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2023-05-04 22:40:13 -0500

    tata

--- BoozeBear/App.js
+++ BoozeBear/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Agregar from './Componentes/Botones_Agregar';
-import Agregar from './Componentes/Botones_Agregar';
+
 
 
 export default function App() {
