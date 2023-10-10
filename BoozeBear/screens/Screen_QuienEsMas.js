28090fe6c6cdead3fd131bf1ab073c5c016ab485
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2023-10-09 23:56:42 -0500

    Lista la base de datos temp

--- BoozeBear/screens/Screen_QuienEsMas.js
+++ BoozeBear/screens/Screen_QuienEsMas.js
@@ -2,12 +2,26 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Agregar from '../Componentes/Btn_Agregar'
 import Informacion from '../Componentes/Modal_info'
+import asks from '../database/Preguntas_QuienEsMas.json'
 
 const Screen_QuienEsMas = ({ navigation }) => {
+
+  // Aqui esta la seleccion de la pregunta
+
+  const [currentQuestion, setCurrentQuestion] = useState();
+  const [level, setLevel] = useState("Quien");
+  const asksQuienEsMas = asks.asksQuienEsMas.filter((ask) => ask.category === level);
+  const muestraQuienEsMas = () => {
+    const random = Math.floor(Math.random() * asksQuienEsMas.length);
+    setCurrentQuestion(asksQuienEsMas[random]?.name);
+  }
+
+  //Llega hasta aqui
 
   const [view, setView] = useState(false)
 
   return (
+    
     <View
       style={{ flex: 1 }}
     >
@@ -16,6 +30,7 @@ const Screen_QuienEsMas = ({ navigation }) => {
         setView={setView}
         text='En la pantalla saldrá el nombre de cada jugador con su respectivo reto o verdad, al terminar dale click  al boton de verdad o reto para que el siguiente jugador lo cumpla.'
       />
+      
       <View
         style={style.superior}
       >
@@ -27,17 +42,37 @@ const Screen_QuienEsMas = ({ navigation }) => {
           text='Información'
           onPress={() => { setView(true); }}
         />
+        
       </View>
+
+        
       <View
         style={style.content}
       >
-        <Text>
-          ¿Cuál es el secreto de la juventud eterna?
-        </Text>
-        <View>
+        <View
+        style={{
+          alignContent: 'center',
+          alignItems: 'center'
+      }}
+
+        >
+          <Text>
+            Pregunta
+          </Text>
+          <Text>
+            {currentQuestion}
+          </Text>
+
+        </View>
+
+        <View
+         style={{
+            flexDirection: 'row'
+          }}
+        >
           <Agregar
-            text='Siguiente'
-            onPress={() => { }}
+            text='Next'
+            onPress={muestraQuienEsMas}
           />
         </View>
       </View>
